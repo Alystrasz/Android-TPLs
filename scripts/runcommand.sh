b81cc44c5e774d9fbeb046e18fa6d05a2557cc93
Rubén Saborido <rsain@uma.es>
2018-08-30 11:45:06 -0400

    Create runcommand.sh
    It runs a command periodically and writes its output in a file.

---
+++ scripts/runcommand.sh
@@ -0,0 +1,16 @@
+#!/sbin/adbdsh
+
+#It runs a command periodically and writes its output in a file.
+#First parameter is the time in seconds between different executions.
+#Second parameter is the command to run.
+#Third parameter is the file which will contain the output.
+
+if [ "$#" -ne 3 ]; then
+    echo "Illegal number of parameters"
+else
+	> $3
+	while :; do 
+		eval $2 >> $3; 
+		sleep $1;
+	done
+fi
