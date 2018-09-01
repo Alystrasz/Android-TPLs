077d73b06ccfe14c3ed4ddf1ac7872ed99da77b6
Rubén Saborido <rsain@uma.es>
2018-09-01 17:24:24 -0400

    Add files via upload
    Experiments for three popular advertising TPLs. It contains the minimal Android apps using these TPLs, the scenarios to play, and the metric values obtained for them.

---
+++ study/Advertising/scenarios/mopub-release.sh
@@ -0,0 +1,10 @@
+NAME="mopub"
+
+PACKAGE="ruben.soccerlab.$NAME"
+ACTIVITY="ruben.soccerlab.$NAME.MainActivity"
+
+# Launch app
+am start -n $PACKAGE/$ACTIVITY
+
+# Wait few seconds to load first ad
+sleep 10
