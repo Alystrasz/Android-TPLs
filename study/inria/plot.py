788acb56697684a0d78b22a2be0d2bb820d88f54
Rémy Raes <remy.raes@inria.fr>
2019-09-06 15:21:33 +0200

    [feat] adding a class to analyse all scenario runs for a single library

--- study/inria/plot.py
+++ study/inria/plot.py
@@ -58,9 +58,48 @@ class Graph:
                                yaxis_title='Power (Watt)')
         self.fig.show()
 
+class ExperimentsAnalyser:
+    def __init__(self, name, count):
+        self.name = name
+        self.experiments = []
+        self.count = count
+        self.load_experiments()
+
+    def load_experiments(self):
+        for i in range(self.count):
+            self.experiments.append(Experiment(self.name, "results/%s-%d.csv" % (self.name, i), "blue"))
+
+    def get_mean_data(self):
+        mean = 0
+
+        for exp in self.experiments:
+            selfmean = 0
+            for point in exp.power:
+                selfmean += float(point)
+            mean += (selfmean/len(exp.power))
+
+        return float(mean)/self.count
+
+    def get_markers(self):
+        max = 0
+        min = 500
+        for exp in self.experiments:
+            for point in exp.power:
+                if point < min: min = point
+                if point > max: max = point
+        return {"min": min, "max": max}
+
+    def print_results(self):
+        print("Library tested: %s" % self.name)
+        print("Number of runs: %d" % self.count)
+        print("Average power withdrawn: %fW" % self.get_mean_data())
+        markers = self.get_markers()
+        print("Min value: %fW, max value: %fW" % (markers['min'], markers['max']))
+        print("------------------------------------------------")
 
 
 def main():
+    """
     graph = Graph("Power evolution of Android libraries")
 
     graph.addExperiment(Experiment("Firebase", "results/firebase-release-0.csv", "firebrick"))
@@ -69,8 +108,17 @@ def main():
 
     # graph.plotExperiments()
     graph.plotSmoothedExperiments()
-
     graph.show()
+    """
+
+    runs_per_scenario = 30
+
+    firebase_analyser = ExperimentsAnalyser("firebase-release", runs_per_scenario)
+    firebase_analyser.print_results()
+    flurry_analyser = ExperimentsAnalyser("flurry-release", runs_per_scenario)
+    flurry_analyser.print_results()
+    google_analyser = ExperimentsAnalyser("google-release", runs_per_scenario)
+    google_analyser.print_results()
 
 if __name__ == "__main__":
     main()
