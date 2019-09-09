72e87fdc3e18eefdb41cc7434f84a5012d04f120
Rémy Raes <remy.raes@inria.fr>
2019-09-09 10:18:08 +0200

    [feat] adding standard deviation measurement

--- study/inria/classes/ExperimentsAnalyser.py
+++ study/inria/classes/ExperimentsAnalyser.py
@@ -1,4 +1,5 @@
 from Experiment import Experiment
+import math
 
 class ExperimentsAnalyser:
     def __init__(self, name, count):
@@ -11,7 +12,7 @@ class ExperimentsAnalyser:
         for i in range(self.count):
             self.experiments.append(Experiment(self.name, "results/%s-%d.csv" % (self.name, i), "blue"))
 
-    def get_mean_data(self):
+    def get_mean(self):
         mean = 0
 
         for exp in self.experiments:
@@ -21,6 +22,25 @@ class ExperimentsAnalyser:
             mean += (selfmean/len(exp.power))
 
         return float(mean)/self.count
+
+    def get_standard_deviation(self):
+        deviation = 0
+
+        for exp in self.experiments:
+            mean = 0
+            distance = 0
+
+            for point in exp.power:
+                mean += point
+            mean = float(mean)/len(exp.power)
+
+            for point in exp.power:
+                distance += (point - mean)**2
+
+            deviation += math.sqrt(distance/len(exp.power))
+
+        return float(deviation)/len(self.experiments)
+
 
     def get_markers(self):
         max = 0
@@ -34,7 +54,8 @@ class ExperimentsAnalyser:
     def print_results(self):
         print("Library tested: %s" % self.name)
         print("Number of runs: %d" % self.count)
-        print("Average power withdrawn: %fW" % self.get_mean_data())
+        print("Average power withdrawn: %fW" % self.get_mean())
         markers = self.get_markers()
         print("Min value: %fW, max value: %fW" % (markers['min'], markers['max']))
+        print("Average standard deviation: %fW" % self.get_standard_deviation())
         print("------------------------------------------------")
